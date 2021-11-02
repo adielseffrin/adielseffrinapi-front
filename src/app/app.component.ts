ccfd3364c60448173dd2d697423edb34770250a1
Adiel Seffrin <adielseffrin@gmail.com>
2021-11-02 11:03:51 -0300

    sending jwt to ebs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
 import {HttpClient, HttpHeaders} from '@angular/common/http';
+import { InfoRequest } from './models/info-request';
 
 @Component({
   selector: 'app-root',
@@ -10,44 +11,48 @@ export class AppComponent implements OnInit  {
   public title = 'pizza';
   public token: string = "";
   public userId: string = "";
+  public userName: string = "";
   public freeSlots = 21;
 
   public ingredientsArray : Array<any> = [];
 
   private twitch = window.Twitch.ext;
   private ingredientsPath: any;
+  
+  private pathToIngredientImage = "assets/img/{ingredient}";
+
 
   constructor(
     private ref: ChangeDetectorRef,
     private http: HttpClient
     ){
-    this.ingredientsPath = () => {
-      const pathToIngredientImage = "assets/img/{ingredient}.png";
-      const ingredients = [
-        "alho",
-        "tomate",
-        "azeite-oleo",
-        "azeitona",
-        "calabresa",
-        "milho",
-        "queijo",
-        "oregano",
-        "pizza-fatia",
-        "pizza",
-      ];
+    // this.ingredientsPath = () => {
+    //   const pathToIngredientImage = "assets/img/{ingredient}.png";
+    //   const ingredients = [
+    //     "alho",
+    //     "tomate",
+    //     "azeite-oleo",
+    //     "azeitona",
+    //     "calabresa",
+    //     "milho",
+    //     "queijo",
+    //     "oregano",
+    //     "pizza-fatia",
+    //     "pizza",
+    //   ];
 
-      const slot = ingredients.map((item) =>
-        ({ingr: pathToIngredientImage.replace("{ingredient}", item), qtde: 3})
-      );
-      return slot;
-    }
+    //   const slot = ingredients.map((item) =>
+    //     ({ingr: pathToIngredientImage.replace("{ingredient}", item), qtde: 3})
+    //   );
+    //   return slot;
+    // }
     this.twitchAuthorize.bind(this);
   }
 
   ngOnInit(){
     this.twitchContext();
     this.twitchAuthorize();
-    this.ingredientsArray = this.ingredientsPath();
+    // this.ingredientsArray = this.ingredientsPath();
     this.freeSlots = this.freeSlots - this.ingredientsArray.length;
 
   }
@@ -62,23 +67,35 @@ export class AppComponent implements OnInit  {
   private twitchAuthorize(){
     this.twitch.onAuthorized((auth:any) => {
       let token = auth.token;
-      
+      //console.log(token);
+      //console.log(auth);
       // let userId = auth.userId;
 
       var parts=auth.token.split(".");
       var payload=JSON.parse(window.atob(parts[1]));
       this.userId = payload.user_id;//trocar pelo nick
       this.ref.detectChanges()
-      this.makeGetRequest(auth.helixToken);
+      this.makeGetRequest(this.userId, token);
+      
     });
   }
 
-  private urlteste = " https://mean-bobcat-15.loca.lt/pizza/info/";
-  private makeGetRequest(id:string){
-    this.http.get(this.urlteste+id)
+  private urlteste = "http://localhost:8000/pizza/info/";
+  private makeGetRequest(id:string, token : string){
+    const httpHeaders: HttpHeaders = new HttpHeaders({
+      JWT: token
+    });
+    this.http.get<InfoRequest>(this.urlteste, {headers : httpHeaders})
     .subscribe(
-      res => console.log(res),
-      err => console.log(err)
+      res => 
+      {
+        this.ingredientsArray = res.ingredientes.map((item) =>
+        ({ingr: this.pathToIngredientImage.replace("{ingredient}", item.name_image_ext), qtde: item.quantidade})
+      );
+      this.userName = res.info.nick;
+      this.ref.detectChanges();
+      }
+      ,err => console.log(err)
     )
   }
   // private makeGetRequest(token : string){
